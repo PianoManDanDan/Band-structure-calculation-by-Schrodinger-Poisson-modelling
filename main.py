31b55cdcd6dcf95b0b3d70beccbc0e063fc9ad38
PianoManDanDan <djm_djm@hotmail.com>
2019-02-04 00:03:21 +0000

    Add menu bar

--- main.py
+++ main.py
@@ -23,13 +23,31 @@ from schrodinger_poisson.schrodinger import solve_schrodinger
 # from schrodinger_poisson.diffusion import solve_diffusion
 
 def create_window():
-    """creates application window"""
+    """creates application window and contains all window functionality"""
+
+    def restart():
+        """Restarts window"""
+        root.destroy()
+        create_window()
+        return
 
     # Window setup
     root = tk.Tk()
     root.title("Band Structure Calculator")
-    root.state('zoomed')
+    # root.state('zoomed')
     root.wm_iconbitmap('icon.ico')
+
+    # Add top menu toolbar
+    menu = tk.Menu(root)
+    root.config(menu=menu)
+    file_menu = tk.Menu(menu, tearoff=False)
+    menu.add_cascade(label="File", menu=file_menu)
+    file_menu.add_command(label='New calculation...', command=restart)
+    file_menu.add_separator()
+    file_menu.add_command(label='Exit', command=root.destroy)
+
+
+
 
 
 
